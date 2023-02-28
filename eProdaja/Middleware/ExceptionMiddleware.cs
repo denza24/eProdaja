0f606bab11b5768e35601d7d7f81d14c39fa3a30
Deni <brkandeni@gmail.com>
2023-02-28 22:16:53 +0100

    exception middleware

---
+++ eProdaja/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+             IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException((int)HttpStatusCode.InternalServerError, "Internal Server Error");
+
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+
+    public class ApiException
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = null!;
+        public string? Details { get; set; }
+
+        public ApiException(int statusCode, string message, string? details = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+
+
+    }
+}
